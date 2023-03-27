949ea8f8fd3b3452269f26bc8c2bf37a9279e79e
Eyüphan Akkaya <96181594+eyuphanakkaya@users.noreply.github.com>
2023-03-27 16:13:47 +0300

    Initial Commit

---
+++ famousInformation/famousInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  famousInformationApp.swift
+//  famousInformation
+//
+//  Created by Eyüphan Akkaya on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct famousInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
